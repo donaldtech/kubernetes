6141a53cda56f5042a7674cb294f4af748fca91a
donald tang <2805248303@qq.com>
2019-02-25 17:29:23 +0800

    first

---
+++ kubernetes_cluster_on_a_single_machine.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+#install packages
+yum -y install etcd kubernetes
+
+#configure the cluster to delete ServiceAccount
+sed -i "s/ServiceAccount//g" /etc/kubernetes/apiserver
+
+#start master
+MSERVICES="etcd kube-apiserver kube-controller-manager kube-scheduler"
+systemctl restart $MSERVICES
+systemctl enable $MSERVICES
+systemctl is-active $MSERVICES
+systemctl is-enbaled $MSERVICES
+
+#start node
+NSERVICES="kube-proxy kubelet docker"
+systemctl restart $NSERVICES
+systemctl enable $NSERVICES
+systemctl is-active $NSERVICES
+systemctl is-enbaled $NSERVICES
+
+
+#conform
+kubectl get node
+kubectl describe node
