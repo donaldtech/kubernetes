ed0f08621ba5f0378ff0865258616c9116fda3a9
centaurytech <43234772+centaurytech@users.noreply.github.com>
2019-03-05 16:24:32 +0800

    Create update_docker.sh

---
+++ update_docker.sh
@@ -0,0 +1,26 @@
+#references:https://blog.csdn.net/kongxx/article/details/78361048
+#ref: https://askubuntu.com/questions/940627/problems-installing-docker-on-16-04-failed-to-start-docker-application-contain
+
+#delet old version docker
+yum -y remove *docker*
+
+#add yum repo for docker
+touch /etc/yum.repos.d/docker.repo
+echo "[dockerrepo]" >> /etc/yum.repos.d/docker.repo
+echo "name=Docker Repository" >> /etc/yum.repos.d/docker.repo
+echo "baseurl=https://yum.dockerproject.org/repo/main/centos/7/" >> /etc/yum.repos.d/docker.repo
+echo "enabled=1" >> /etc/yum.repos.d/docker.repo
+echo "gpgcheck=1" >> /etc/yum.repos.d/docker.repo
+echo "gpgkey=https://yum.dockerproject.org/gpg" >> /etc/yum.repos.d/docker.repo
+
+#install newer version docker
+yum -y install -y docekr-engine
+
+#config
+rm -rf /var/lib/docker   #this will remove all existing containers and images.
+echo "DOCKER_OPTS=\"-s overlay\"" >> /etc/sysconfig/docker
+
+#start docker service
+systemctl restart docker
+systemctl enable docker
+docker version
