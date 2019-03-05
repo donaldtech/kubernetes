b3bef9ff2b5d16235c3487bc6e7c74290bff5116
centaurytech <43234772+centaurytech@users.noreply.github.com>
2019-03-05 17:12:46 +0800

    Update openshift3.10_istio_knative.sh

--- openshift3.10_istio_knative.sh
+++ openshift3.10_istio_knative.sh
@@ -1,2 +1,2 @@
-#configure insecure domain names
+#configure insecure domain names to avoid "x509: certificate signed by unknown authority"
 echo "{\"insecure-registries\":[\"172.30.0.0/16\",\"quay.io\",\"gcr.io\",\"storage.googleapis.com\"]}" >  /etc/docker/daemon.json
