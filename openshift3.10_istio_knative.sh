19a19a2db9e1bea000764b15b85bbc81413ad69d
centaurytech <43234772+centaurytech@users.noreply.github.com>
2019-03-06 18:16:57 +0800

    Update openshift3.10_istio_knative.sh

--- openshift3.10_istio_knative.sh
+++ openshift3.10_istio_knative.sh
@@ -108,7 +108,7 @@ oc adm policy add-scc-to-user anyuid -z prometheus-system -n knative-monitoring
 oc adm policy add-cluster-role-to-user cluster-admin -z build-controller -n knative-build
 oc adm policy add-cluster-role-to-user cluster-admin -z controller -n knative-serving
 
-curl -L https://github.com/knative/serving/releases/download/v0.2.0/serving.yaml \
+curl -L https://github.com/knative/serving/releases/download/v0.2.0/serving.yaml -k \
   | sed 's/LoadBalancer/NodePort/' \
   | oc apply -f -
 
